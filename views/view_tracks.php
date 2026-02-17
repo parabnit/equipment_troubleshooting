efc5b78b0e0dd09363d271679471b8b64c982f48
parabnit <parabnitin51@gmail.com>
2026-02-17 17:09:34 +0530

    changes completed

--- views/view_tracks.php
+++ views/view_tracks.php
@@ -228,12 +228,12 @@ file-attach,
   <h5><b>Created by:</b> <?= getName($complaint['member_id']); ?></h5>
   <p><b>Time of Complaint:</b> <?= display_timestamp($complaint['time_of_complaint']); ?></p>
 
- <p>
-  <b>Tool Name:</b>
-  <span style="color:#0033FF;">
-    <?= getComplaintComponentName($complaint); ?>
-  </span>
-</p>
+  <p>
+    <b>Tool Name:</b>
+    <span style="color:#0033FF;">
+      <?= getComplaintComponentName($complaint); ?>
+    </span>
+  </p>
 
   <?php if (!empty($complaint['allocated_to'])): ?>
     <p><b>Allocated To:</b> <?= getName($complaint['allocated_to']); ?></p>
